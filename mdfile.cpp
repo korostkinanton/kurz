459b907b5f7064817fde52378d6b7fcb6c11ce1b
korostkinanton <akorostkin1@gmail.com>
2023-11-13 11:54:07 +0300

    Чел...

---
+++ mdfile.cpp
@@ -0,0 +1,145 @@
+#include "mdfile.h"
+
+int Server::self_addr(string error, string file_error, int port){
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in * self_addr = new (sockaddr_in);
+    self_addr->sin_family = AF_INET;
+    self_addr->sin_port = htons(port);
+    self_addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+    cout << "Ожидание клиента...\n";
+    int b = bind(s,(const sockaddr*) self_addr,sizeof(sockaddr_in));
+        if(b == -1) {
+            cout << "Binding error\n";
+            error = "error binding";
+            errors(error, file_error);
+            return 1;
+            }
+    listen(s, SOMAXCONN);
+return s;
+}
+
+int Server::client_addr(int s, string error, string file_error){
+    sockaddr_in * client_addr = new sockaddr_in;
+    socklen_t len = sizeof (sockaddr_in);
+    int work_sock = accept(s, (sockaddr*)(client_addr), &len);
+    if(work_sock == -1) {
+        cout << "Error #2\n";
+        error = "error #2";
+        errors(error, file_error);
+        return 1;
+    }
+    else {
+        //Успешное подключение к серверу
+        cout << "Клиент подключился!\n";
+        return work_sock;
+    }
+    }
+
+int autorized(int work_sock, string file_name, string file_error){
+
+        string ok = "OK";
+        string salt = "1111111111111111";
+        string err = "ERR";
+        string error;
+        char msg[255];
+        
+        //Авторизация
+        int size;
+        size = recv(work_sock, &msg, sizeof(msg), 0);
+            string message(msg, size);
+    string login, hashq;
+    fstream file;
+    file.open(file_name);
+    getline (file, login, ':');
+    getline (file, hashq);
+
+            //СВЕРКА ЛОГИНОВ
+            if(message != login){
+    msgsend(work_sock,  err);
+    error = "Ошибка логина";
+    errors(error, file_error);
+    close(work_sock);
+    return 1;
+            }else{
+            //соль отправленная клиенту
+            msgsend(work_sock,  salt);
+            size = recv(work_sock, msg, sizeof(msg), 0);
+            string H(msg, size);
+            string sah = salt + hashq;
+            string digest;
+            digest = MD(sah);
+            //СВЕРКА ПАРОЛЕЙ
+            if(digest != H){
+    cout << digest << endl;
+    cout << H << endl;
+    msgsend(work_sock,  err);
+    error = "Ошибка пароля";
+    errors(error, file_error);
+    close(work_sock);
+    return 1;
+            }else{
+    msgsend(work_sock,  ok);
+            }
+}
+return 1;
+}
+
+void msgsend(int work_sock, string mess){
+    char *buffer = new char[4096];
+    strcpy(buffer, mess.c_str());
+    send(work_sock, buffer, mess.length(), 0);
+}
+
+
+
+string MD(string sah){
+Weak::MD5 hash;
+    string digest;
+    StringSource(sah, true,  new HashFilter(hash, new HexEncoder(new StringSink(digest))));  // строка-приемник
+      return digest;
+ }
+void errors(string error, string name){
+    ofstream file;
+    file.open(name, ios::app);
+    if(file.is_open()){
+        time_t seconds = time(NULL);
+        tm* timeinfo = localtime(&seconds);
+        file<<error<<':'<<asctime(timeinfo)<<endl;
+        cout << "error: " << error << endl;
+    }
+}
+int er(string file_name, string file_error){
+        fstream file;
+        file.exceptions(ifstream::badbit | ifstream::failbit);
+        try{
+        file.open(file_name);
+        return 1;
+        }catch(const exception & ex){
+        string error = "error open file";
+        errors(error, file_error);
+        return 12;
+        }
+        }
+
+int math(int work_sock){
+    uint32_t kolvo;
+    uint32_t numb;
+    uint64_t vect;
+        recv(work_sock, &kolvo, sizeof(kolvo), 0);
+    //цикл векторов
+    for(uint32_t j=0; j<kolvo; j++){
+        recv(work_sock, &numb, sizeof(numb), 0);//прием длинны для первого вектора
+        uint32_t sum = 0;
+    //цикл значений
+    for(uint32_t i=0; i<numb; i++){
+        recv(work_sock, &vect, sizeof(vect), 0);
+        sum = sum+vect;
+    }
+    uint64_t mfc;
+    mfc = sum;
+    send(work_sock, &mfc, sizeof(mfc), 0);
+    } 
+cout << "Program finish!" <<endl;
+close(work_sock);
+return 1;
+}
---
+++ mdfile.h
@@ -0,0 +1,35 @@
+#include <netinet/in.h>
+#include <iostream>
+#include <cassert>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cryptopp/cryptlib.h>
+#include <iostream>
+#include <vector>
+#include <getopt.h>
+#include <cryptopp/hex.h> // HexEncoder
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+using namespace CryptoPP;
+using namespace std;
+#include <cryptopp/md5.h> // MD%, для другого алгоритма - другой заголовочный файл
+string MD(string sah);
+void errors(string error, string name);
+int er(string file_name, string file_error);
+void msgsend(int work_sock, string mess);
+int autorized(int work_sock, string file_name, string file_error);
+class Server{
+private:
+
+public:
+int self_addr(string error, string file_error, int port);
+int client_addr(int s, string error, string file_error);
+};
+
+int math(int work_sock);
+
+
